805913f8d8e4c80a9391730fce8b63b73a61f3d9
Gideon Sam <gideonokinyi@gmail.com>
2022-03-15 22:57:03 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Write a program that prints _putchar, followed by a new line.
+ * print_alphabet - check the code for Holberton School students.
  *
- * Return: always 0 (Success)
+ * Return: Always 0.
  */
-int main(void)
+
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar (c);
+		c++;
+
+	}
+	_putchar ('\n');
+
 }
